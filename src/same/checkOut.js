ae3477e6665f6bf1969069047190ce9bb5458937
paretkf <paretk7@gmail.com>
2018-06-06 19:59:40 +0700

    refac code

--- src/same/checkOut.js
+++ src/same/checkOut.js
@@ -1,21 +1,19 @@
-function calculate (item, promotion) {
-  item.sort((a, b) => (b.price - a.price))
-  var case5 = (item.length / 5 | 0) * 5 // find amount of case 5 item
-  var case3 = ((item.length - case5) / 3 | 0) * 3 // find amount of case 3 item
+function calculate (items, promotions) {
+  items.sort((a, b) => (b.price - a.price))
+  var itemInTier5 = (items.length / 5 | 0) * 5 // find amount of case 5 item
+  var itemInTier3 = ((items.length - itemInTier5) / 3 | 0) * 3 // find amount of case 3 item
 
-  item.slice(0, case5).map(item => {
-    item.priceDiscount = promotion[item.price].tier5
+  items = setPriceDiscount(0, itemInTier5, items, promotions, 'tier5')
+  items = setPriceDiscount(itemInTier5, itemInTier5 + itemInTier3, items, promotions, 'tier3')
+  items = setPriceDiscount(itemInTier5 + itemInTier3, items.length, items, promotions, 'tier1')
+  return items
+}
+function setPriceDiscount (start, end, items, promotions, tier) {
+  items.slice(start, end).map(item => {
+    item.priceDiscount = promotions[item.price][tier]
     return item
   })
-  item.slice(case5, case5 + case3).map(item => {
-    item.priceDiscount = promotion[item.price].tier3
-    return item
-  })
-  item.slice(case5 + case3, item.length).map(item => {
-    item.priceDiscount = item.price
-    return item
-  })
-  return item
+  return items
 }
 
 module.exports = calculate
--- src/same/checkOut.test.js
+++ src/same/checkOut.test.js
@@ -1,19 +1,19 @@
 const test = require('ava')
 const checkOut = require('./checkOut')
-const promotion = require('./promotion.json')
+const promotions = require('./promotions.json')
 
 test('1 return 100 ', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 }
   ]
   var result = [
     { id: '001', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('2 return 100 200 ', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 }
   ]
@@ -21,11 +21,11 @@ test('2 return 100 200 ', function (t) {
     { id: '002', name: 'B', price: 200, priceDiscount: 200 },
     { id: '001', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('3 return 260 170 80', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 }
@@ -35,11 +35,11 @@ test('3 return 260 170 80', function (t) {
     { id: '002', name: 'B', price: 200, priceDiscount: 170 },
     { id: '001', name: 'A', price: 100, priceDiscount: 80 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('4 return 450 260 170 100', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -51,11 +51,11 @@ test('4 return 450 260 170 100', function (t) {
     { id: '002', name: 'B', price: 200, priceDiscount: 170 },
     { id: '001', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('5 return 400 220 150 60 60', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -69,11 +69,11 @@ test('5 return 400 220 150 60 60', function (t) {
     { id: '001', name: 'A', price: 100, priceDiscount: 60 },
     { id: '006', name: 'A', price: 100, priceDiscount: 60 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('6 return 400 220 150 60 60 100', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -89,11 +89,11 @@ test('6 return 400 220 150 60 60 100', function (t) {
     { id: '006', name: 'A', price: 100, priceDiscount: 60 },
     { id: '007', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('7 return 400 220 150 150 60 100 100', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -111,11 +111,11 @@ test('7 return 400 220 150 150 60 100 100', function (t) {
     { id: '006', name: 'A', price: 100, priceDiscount: 100 },
     { id: '007', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('8 return 400 400 220 150 150 80 80 80', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -135,11 +135,11 @@ test('8 return 400 400 220 150 150 80 80 80', function (t) {
     { id: '006', name: 'A', price: 100, priceDiscount: 80 },
     { id: '007', name: 'A', price: 100, priceDiscount: 80 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('12 return 400 400 220 220 150 150 60 60 60 60 100 100', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -167,11 +167,11 @@ test('12 return 400 400 220 220 150 150 60 60 60 60 100 100', function (t) {
     { id: '001', name: 'A', price: 100, priceDiscount: 100 },
     { id: '001', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('13', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -201,11 +201,11 @@ test('13', function (t) {
     { id: '001', name: 'A', price: 100, priceDiscount: 80 },
     { id: '001', name: 'A', price: 100, priceDiscount: 80 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('9 return 400 400 220 150 150 170 80 80 100', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -227,11 +227,11 @@ test('9 return 400 400 220 150 150 170 80 80 100', function (t) {
     { id: '006', name: 'A', price: 100, priceDiscount: 80 },
     { id: '007', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('11 return 400 400 400 220 220 150 150 150  60 60 100', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -257,11 +257,11 @@ test('11 return 400 400 400 220 220 150 150 150  60 60 100', function (t) {
     { id: '001', name: 'A', price: 100, priceDiscount: 60 },
     { id: '007', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('13 return 400 400 400 220 220 150 150 150 150 60 80 80 80', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -291,10 +291,10 @@ test('13 return 400 400 400 220 220 150 150 150 150 60 80 80 80', function (t) {
     { id: '011', name: 'A', price: 100, priceDiscount: 80 },
     { id: '001', name: 'A', price: 100, priceDiscount: 80 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 test('10 return 400 400 220 220 150 150 60 60 60 60', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -318,11 +318,11 @@ test('10 return 400 400 220 220 150 150 60 60 60 60', function (t) {
     { id: '011', name: 'A', price: 100, priceDiscount: 60 },
     { id: '007', name: 'A', price: 100, priceDiscount: 60 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('12 return 400 400 400 220 220 150 150 60 60 60 100 100', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -350,11 +350,11 @@ test('12 return 400 400 400 220 220 150 150 60 60 60 100 100', function (t) {
     { id: '001', name: 'A', price: 100, priceDiscount: 100 },
     { id: '007', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('15 return 400 400 400 400 220 220 220 150 150 150 150 60 60 60 60', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -388,11 +388,11 @@ test('15 return 400 400 400 400 220 220 220 150 150 150 150 60 60 60 60', functi
     { id: '006', name: 'A', price: 100, priceDiscount: 60 },
     { id: '001', name: 'A', price: 100, priceDiscount: 60 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('14 return 400 400 400 220 220 220 150 150 150 60 80 80 80 100', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -424,11 +424,11 @@ test('14 return 400 400 400 220 220 220 150 150 150 60 80 80 80 100', function (
     { id: '006', name: 'A', price: 100, priceDiscount: 80 },
     { id: '001', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 
 test('19 return 400 400 400 400 220 220 220 220 150 150 150 150 60 60 60 80 80 80 100', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -470,10 +470,10 @@ test('19 return 400 400 400 400 220 220 220 220 150 150 150 150 60 60 60 80 80 8
     { id: '007', name: 'A', price: 100, priceDiscount: 80 },
     { id: '001', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })
 test('24 return 400 400 400 400 220 220 220 220 150 150 150 150 60 60 60 80 80 80 100', function (t) {
-  var item = [
+  var items = [
     { id: '001', name: 'A', price: 100 },
     { id: '002', name: 'B', price: 200 },
     { id: '003', name: 'C', price: 300 },
@@ -525,5 +525,5 @@ test('24 return 400 400 400 400 220 220 220 220 150 150 150 150 60 60 60 80 80 8
     { id: '001', name: 'A', price: 100, priceDiscount: 80 },
     { id: '001', name: 'A', price: 100, priceDiscount: 100 }
   ]
-  t.deepEqual(checkOut(item, promotion), result)
+  t.deepEqual(checkOut(items, promotions), result)
 })

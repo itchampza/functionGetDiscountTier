51ec651df37822048a721def0f205cf0c8a1d003
paretkf <paretk7@gmail.com>
2018-06-06 19:26:16 +0700

    before function

--- src/same/checkOut.js
+++ src/same/checkOut.js
@@ -2,15 +2,19 @@ function calculate (item, promotion) {
   item.sort((a, b) => (b.price - a.price))
   var case5 = (item.length / 5 | 0) * 5 // find amount of case 5 item
   var case3 = ((item.length - case5) / 3 | 0) * 3 // find amount of case 3 item
-  for (var i = 0; i < item.length; i++) {
-    if (i < case5) {
-      item[i].priceDiscount = promotion.find(pro => pro.price === item[i].price).promotion.tier5
-    } else if (i < (case5 + case3)) {
-      item[i].priceDiscount = promotion.find(pro => pro.price === item[i].price).promotion.tier3
-    } else {
-      item[i].priceDiscount = item[i].price
-    }
-  }
+
+  item.slice(0, case5).map(item => {
+    item.priceDiscount = promotion[item.price].tier5
+    return item
+  })
+  item.slice(case5, case5 + case3).map(item => {
+    item.priceDiscount = promotion[item.price].tier3
+    return item
+  })
+  item.slice(case5 + case3, item.length).map(item => {
+    item.priceDiscount = item.price
+    return item
+  })
   return item
 }
 
